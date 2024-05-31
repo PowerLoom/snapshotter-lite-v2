e29a6fe9936fe8abe99ebd3ea42c17531ac7f88b
muku314115 <mukangotan@gmail.com>
2024-05-31 19:05:50 +0800

    updated contract addresses

--- snapshotter_autofill.sh
+++ snapshotter_autofill.sh
@@ -5,7 +5,7 @@ source .env
 if [ -z "$OVERRIDE_DEFAULTS" ]; then
     echo "reset to default values...";
     export PROST_RPC_URL="https://rpc-prost1h-proxy.powerloom.io"
-    export PROTOCOL_STATE_CONTRACT="0x5aE5516cDC884B54D933A8d552898407cafe9C36"
+    export PROTOCOL_STATE_CONTRACT="0xd54cbB90da2C049a5D5d47E1be3f19b473552D34"
     export PROST_CHAIN_ID="11165"
 fi
 
