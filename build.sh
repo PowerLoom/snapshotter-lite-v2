d9db88dde1eb0e579f6b0c56f868a32b02df8644
muku314115 <mukangotan@gmail.com>
2024-06-04 06:31:54 +0000

    Updated contract address and sequencer id

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ source .env
 if [ -z "$OVERRIDE_DEFAULTS" ]; then
     echo "reset to default values...";
     export PROST_RPC_URL="https://rpc-prost1h-proxy.powerloom.io"
-    export PROTOCOL_STATE_CONTRACT="0xd54cbB90da2C049a5D5d47E1be3f19b473552D34"
+    export PROTOCOL_STATE_CONTRACT="0xF529C1cCE6a93aDA3dEB32dEEB8A2980D09DDc8C"
     export PROST_CHAIN_ID="11165"
 fi
 
