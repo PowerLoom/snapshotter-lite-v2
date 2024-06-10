632b9b39f873e04a3136fb09c479c53c2ba6d2d4
muku314115 <mukangotan@gmail.com>
2024-06-10 13:55:37 +0530

    updating branch name for testing

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ echo "setting up codebase...";
 rm -rf snapshotter-lite-local-collector;
 git clone https://github.com/PowerLoom/snapshotter-lite-local-collector.git;
 cd ./snapshotter-lite-local-collector;
-git checkout simulation_node_bi;
+git checkout consensus_complete;
 cd ..;
 
 echo "bootstrapping complete!";
