fb49d28ecc5c902993b1d871badb73e5485292ba
anomit ghosh <anomit.ghosh@gmail.com>
2024-07-01 20:51:17 +0530

    chore: first commit that supports passing custom listening port for local collector

--- build.sh
+++ build.sh
@@ -38,12 +38,12 @@ fi
 
 source .env
 
-if [ -z "$OVERRIDE_DEFAULTS" ]; then
-    echo "reset to default values...";
-    export PROST_RPC_URL="https://rpc-prost1h-proxy.powerloom.io"
-    export PROTOCOL_STATE_CONTRACT="0x10c5E2ee14006B3860d4FdF6B173A30553ea6333"
-    export PROST_CHAIN_ID="11165"
-fi
+# if [ -z "$OVERRIDE_DEFAULTS" ]; then
+#     echo "reset to default values...";
+#     export PROST_RPC_URL="https://rpc-prost1h-proxy.powerloom.io"
+#     export PROTOCOL_STATE_CONTRACT="0x10c5E2ee14006B3860d4FdF6B173A30553ea6333"
+#     export PROST_CHAIN_ID="11165"
+# fi
 
 echo "testing before build...";
 
@@ -80,6 +80,10 @@ fi
 
 if [ "$PROTOCOL_STATE_CONTRACT" ]; then
     echo "Found PROTOCOL_STATE_CONTRACT ${PROTOCOL_STATE_CONTRACT}";
+fi
+
+if [ "$LOCAL_COLLECTOR_PORT" ]; then
+    echo "Found LOCAL_COLLECTOR_PORT ${LOCAL_COLLECTOR_PORT}";
 fi
 
 if [ "$RELAYER_HOST" ]; then
