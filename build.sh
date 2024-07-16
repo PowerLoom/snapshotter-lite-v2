6e7b50324be09fd1d9f9a97254b10873f8f6b8e4
anomit ghosh <anomit.ghosh@gmail.com>
2024-07-16 12:31:17 +0530

    chore: set default contract address in build.sh setup

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ source .env
 if [ -z "$OVERRIDE_DEFAULTS" ]; then
     echo "reset to default values...";
     export PROST_RPC_URL="https://rpc-prost1h-proxy.powerloom.io"
-    export PROTOCOL_STATE_CONTRACT="0x1996e00549a7b664E927F872342cE66D2545869c"
+    export PROTOCOL_STATE_CONTRACT="0x10c5E2ee14006B3860d4FdF6B173A30553ea6333"
     export PROST_CHAIN_ID="11165"
 fi
 
@@ -123,10 +123,11 @@ echo "Current branch is ${GIT_BRANCH}";
 
 #if on main git branch, set image_tag to latest or use the branch name
 
-if [ "$GIT_BRANCH" = "main" ]; then
-    export IMAGE_TAG="latest";
+
+if [ "$GIT_BRANCH" = "dockerify" ]; then
+    export IMAGE_TAG="dockerify"
 else
-    export IMAGE_TAG="${GIT_BRANCH}";
+    export IMAGE_TAG="latest"
 fi
 
 echo "Building image with tag ${IMAGE_TAG}";
