d56c45c9d3f9cbccbf2e9aececf3aed5cbbc352e
anomit ghosh <anomit.ghosh@gmail.com>
2024-07-16 12:30:53 +0530

    chore: reverting protocol state changes from last release

--- snapshotter_autofill.sh
+++ snapshotter_autofill.sh
@@ -5,7 +5,7 @@ source .env
 if [ -z "$OVERRIDE_DEFAULTS" ]; then
     echo "reset to default values...";
     export PROST_RPC_URL="https://rpc-prost1h-proxy.powerloom.io"
-    export PROTOCOL_STATE_CONTRACT="0x1996e00549a7b664E927F872342cE66D2545869c"
+    export PROTOCOL_STATE_CONTRACT="0x10c5E2ee14006B3860d4FdF6B173A30553ea6333"
     export PROST_CHAIN_ID="11165"
 fi
 
