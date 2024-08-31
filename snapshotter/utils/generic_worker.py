4e9fb74e814cb23a64208c18edb458a164e4a9fd
anomit ghosh <anomit.ghosh@gmail.com>
2024-08-31 18:56:33 +0530

    wip: attempt to connect to local collector over loopback interface

--- snapshotter/utils/generic_worker.py
+++ snapshotter/utils/generic_worker.py
@@ -589,7 +589,7 @@ class GenericAsyncWorker:
 
     async def _init_grpc(self):
         self._grpc_channel = Channel(
-            host='host.docker.internal',
+            host='127.0.0.1',
             port=settings.local_collector_port,
             ssl=False,
         )
