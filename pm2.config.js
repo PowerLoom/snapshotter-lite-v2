4f4ddc63eb77137c8682038bc1c230491fa16abd
Seth <seth@powerloom.io>
2025-01-16 22:18:22 -0500

    chore: optional cron restart

--- pm2.config.js
+++ pm2.config.js
@@ -1,6 +1,6 @@
 // this means if app restart {MAX_RESTART} times in 1 min then it stops
 const NODE_ENV = process.env.NODE_ENV || 'development';
-
+const CRON_RESTART = process.env.CRON_RESTART || 'false';
 const MAX_RESTART = 3;
 const MIN_UPTIME = 60000;
 
@@ -29,7 +29,7 @@ module.exports = {
       env: {
         NODE_ENV: NODE_ENV,
       },
-      cron_restart: "0 * * * *",
+      ...(CRON_RESTART === 'true' ? { cron_restart: "0 * * * *" } : {}),
       autorestart: true,
       kill_timeout: 5000,
       stop_exit_codes: [0, 143],
