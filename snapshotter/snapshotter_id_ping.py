8df9330cb2f487e67861db4235dbcf73004b051d
anomit ghosh <anomit.ghosh@gmail.com>
2025-02-02 21:54:15 +0530

    fix: use snapshotter address from private key for identity verification

--- snapshotter/snapshotter_id_ping.py
+++ snapshotter/snapshotter_id_ping.py
@@ -21,9 +21,12 @@ async def main():
     w3 = Web3(Web3.HTTPProvider(settings.anchor_chain_rpc.full_nodes[0].url))
 
     protocol_state_contract = w3.eth.contract(address=settings.protocol_state.address, abi=protocol_abi)
+    # get snapshotter address from private key
+    snapshotter_address = w3.eth.account.private_key_to_account(settings.signer_private_key).address
+    print('Extracted snapshotter address from private key: ', snapshotter_address)
     # Query allowed snapshotters
     allowed_snapshotters = protocol_state_contract.functions.allSnapshotters(
-        Web3.to_checksum_address(settings.instance_id)
+        Web3.to_checksum_address(snapshotter_address)
     ).call()
 
     if allowed_snapshotters is True or allowed_snapshotters:
@@ -39,7 +42,7 @@ async def main():
 
     try:
         slot_id_snapshotter_addr = Web3.to_checksum_address(slot_id_mapping_query)
-        if slot_id_snapshotter_addr == Web3.to_checksum_address(settings.instance_id):
+        if slot_id_snapshotter_addr == Web3.to_checksum_address(snapshotter_address):
             print('Snapshotter identity found in slot ID mapping...')
         else:
             print('Snapshotter identity not found in slot ID mapping...')
