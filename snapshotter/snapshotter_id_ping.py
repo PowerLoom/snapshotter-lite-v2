22170142ed13c04bc67a62cb4a4a53c02e44be1d
Seth <seth@powerloom.io>
2025-04-10 14:19:47 +0530

    chore: update snapshotter id ping to use new protocol state contract and streamline block fetching logic

--- snapshotter/snapshotter_id_ping.py
+++ snapshotter/snapshotter_id_ping.py
@@ -14,54 +14,33 @@ async def main():
     If snapshotting is not allowed, sets the active status key in Redis to False and exits with code 1.
     """
     # Load protocol state ABI
-    protocol_abi = read_json_file(settings.protocol_state_old.abi)
-    print('abi file ', settings.protocol_state_old.abi)
-    print('Contract address: ', settings.protocol_state_old.address)
+    protocol_abi = read_json_file(settings.protocol_state.abi)
+    print('abi file ', settings.protocol_state.abi)
+    print('Contract address: ', settings.protocol_state.address)
 
-    w3_old = Web3(Web3.HTTPProvider(settings.prost_chain_rpc.full_nodes[0].url))
     w3 = Web3(Web3.HTTPProvider(settings.powerloom_chain_rpc.full_nodes[0].url))
     
     try:
-        block_number = w3_old.eth.get_block_number()
+        block_number = w3.eth.get_block_number()
         print(f"✅ Successfully fetched the latest block number {block_number}. Your ISP is supported!")
     except Exception as e:
         print("❌ Failed to fetch the latest block number. Your ISP/VPS region is not supported ⛔️")
         sys.exit(1)
 
-    protocol_state_contract_old = w3_old.eth.contract(address=settings.protocol_state_old.address, abi=protocol_abi)
     protocol_state_contract = w3.eth.contract(address=settings.protocol_state.address, abi=protocol_abi)
-
-    current_epoch_old = protocol_state_contract_old.functions.currentEpoch(settings.old_data_market).call()
-    latest_epoch_id_old = current_epoch_old[2]
-    print('Latest epoch ID detected on old chain: ', latest_epoch_id_old)
-
-    snapshotter_address = w3_old.eth.account.from_key(settings.signer_private_key).address
+    snapshotter_address = w3.eth.account.from_key(settings.signer_private_key).address
     print('Extracted snapshotter address from private key: ', snapshotter_address)
 
-    if latest_epoch_id_old < settings.switch_rpc_at_epoch_id:
-        print('Using old chain')
+    print('Querying allowed snapshotters...')
+    # Query allowed snapshotters
+    allowed_snapshotters = protocol_state_contract.functions.allSnapshotters(
+        Web3.to_checksum_address(snapshotter_address)
+    ).call()
 
-        # Query allowed snapshotters
-        allowed_snapshotters = protocol_state_contract_old.functions.allSnapshotters(
-            Web3.to_checksum_address(snapshotter_address)
-        ).call()
-
-        # Check slot ID mapping
-        slot_id_mapping_query = protocol_state_contract_old.functions.slotSnapshotterMapping(
-            settings.slot_id
-        ).call()
-
-    else:
-        print('Using new chain')
-        # Query allowed snapshotters
-        allowed_snapshotters = protocol_state_contract.functions.allSnapshotters(
-            Web3.to_checksum_address(snapshotter_address)
-        ).call()
-
-        # Check slot ID mapping
-        slot_id_mapping_query = protocol_state_contract.functions.slotSnapshotterMapping(
-            settings.slot_id
-        ).call()
+    # Check slot ID mapping
+    slot_id_mapping_query = protocol_state_contract.functions.slotSnapshotterMapping(
+        settings.slot_id
+    ).call()
 
     if allowed_snapshotters is True or allowed_snapshotters:
         print('✅ Snapshotter identity found in allowed snapshotters...')
